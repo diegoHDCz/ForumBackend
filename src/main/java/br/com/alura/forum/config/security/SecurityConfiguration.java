08503b6d11bb8fd50b297c8ff2ad55b782fc180e
Diego Czajka <diego.czajka@gmail.com>
2022-11-05 16:16:19 -0300

    include swagger

--- src/main/java/br/com/alura/forum/config/security/SecurityConfiguration.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfiguration.java
@@ -63,7 +63,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-
+        web.ignoring().antMatchers(
+                "/**.html", "/v2/api-docs",
+                "/webjars/**", "/configuration/**", "/swagger-resources/**");
 
     }
 
